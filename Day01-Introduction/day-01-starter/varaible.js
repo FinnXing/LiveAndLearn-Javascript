9a8e1b4410eba7b6a1307a7c8ab1933c33f15d20
xingfei <xingfei@weathercn.com>
2021-02-25 18:11:09 +0800

    day 01 html js

---
+++ Day01-Introduction/day-01-starter/varaible.js
@@ -0,0 +1,13 @@
+// 未定义变量
+let firstUndefined;
+console.log('firstUndefined：', firstUndefined)
+
+let firstName = 'ZhangSan'
+const age1 = 100
+let isMarried = true
+/* 
+  也可以使用逗号分隔一行声明
+*/
+let _name = 'a',
+    $job = 'FE';
+console.log(firstName, age1, isMarried, _name, $job)
